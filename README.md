c746bd8ea067aa6b74d6f6bb0599e47f4a4cf0a1
Murilo Belletato <murilo.belletato@gmail.com>
2014-05-25 11:26:17 -0300

    first commit

--- README.md
+++ README.md
@@ -11,21 +11,28 @@ The run_analysis.R script works as following:
 5. Bind (Column Bind) TestSet[TEST] and TrainingSet[TRAIN].
 6. Create one Tidy DataSet[DATA] binding (Row Bind) both TEST and TRAIN.
 7. Merge DATA and ACTIVITY into a complete DataSet.
-8. Write a file "tidyData.txt" using ";" as column separator.
+8. Melt DATA
+9. DCast melted DATA 
+10. Write a file "tidyData.txt" using ";" as column separator.
 
 
+After executing the analysis, 
 
 ###Scripts
 *run_analysis.R: R script performing the Course Project analysis
-*data/: Main data folder containing Samsung data
+*tidyData.txt: Final DataSet (Course Project - step 5)
+*tidyData_1.txt: Final DataSet (Course Project - step 5)
+*./data/: Main data folder containing Samsung data
 
 
 ###Data Frames
 * MEANSTD_OK: Means and Standard Deviation measures
-* ACTIVITY : Activity Labels
-* TEST_V : Test Set (Values)
-* TEST_L : Test Set (Labels)
-* TEST : Test Set
-* TRAIN_V : Training Set (Values)
-* TRAIN_L : Training Set (Labels)
-* TRAIN : Training Set
+* ACTIVITY: Activity Labels
+* TEST_V: Test Set (Values)
+* TEST_L: Test Set (Labels)
+* TEST: Test Set
+* TRAIN_V: Training Set (Values)
+* TRAIN_L: Training Set (Labels)
+* TRAIN: Training Set
+* MELTDATA: Melted DataSet
+* TIDYDATA: Final DataSet (Variable Means ~ Subject + Activity)
--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,18 @@
+library(reshape2)
+
 ## MEANSTD_OK   : Means and Standard Deviation measures
 ## ACTIVITY     : Activity Labels
 ## TEST_V       : Test Set (Values)
 ## TEST_L       : Test Set (Labels)
+## TEST_S       : Test Set (Subject)
 ## TEST         : Test Set
 ## TRAIN_V      : Training Set (Values)
 ## TRAIN_L      : Training Set (Labels)
+## TRAIN_S      : Training Set (Subject)
 ## TRAIN        : Training Set
+## MELTDATA     : Melted DataSet
+## TIDYDATA     : Final DataSet (Variable Means ~ Subject + Activity)
+
 
 MEANSTD_OK <- c(1,2,3,4,5,6,41,42,43,44,45,46,81,82,83,84,85,86,
                 121,122,123,124,125,126,161,162,163,164,165,166,
@@ -29,15 +36,28 @@ TRAIN_V <- TRAIN_V[MEANSTD_OK]
 TEST_L <- read.table("./data//test//y_test.txt")
 TRAIN_L <- read.table("./data//train/y_train.txt")
 
+##Load TrainingSet and TestSet Subjects
+TEST_S <- read.table("./data//test//subject_test.txt", col.names=c("Subject"))
+TRAIN_S <- read.table("./data//train/subject_train.txt", col.names=c("Subject"))
+
 ##Bind TestSet and TrainingSet
-TEST <- cbind(TEST_V, TEST_L)
-TRAIN <- cbind(TRAIN_V, TRAIN_L)
+TEST <-  cbind(TEST_S, TEST_V,  TEST_L)
+TRAIN <- cbind(TRAIN_S, TRAIN_V, TRAIN_L)
 
 ##Create one DataSet (TrainigSet + DataSet)
 DATA <- rbind(TEST, TRAIN)
+
+##Subject as factor\
+DATA$Subject <- as.factor(DATA$Subject)
 
 ##Label Activity 
 DATA <- merge(ACTIVITY, DATA, by="V1")
 names(DATA)[1:2] <- c("ActivityID", "Activity")
 
-write.table(DATA, file="tidyData.txt", sep=";", row.names=F)
+##Melt DATA
+MELTDATA <- melt(DATA, id=c("Subject", "Activity"), measure.vars=c(4:69))
+
+##DCast melted DATA
+TIDYDATA <- dcast(MELTDATA, Subject + Activity ~ variable, mean)
+
+write.table(TIDYDATA, file="tidyData.txt", sep=";", row.names=F)
